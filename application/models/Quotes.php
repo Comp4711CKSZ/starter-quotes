ad9b524f8848aec04404d1ace9b8de6ef9994c6d
CK Song <badukboy@gmail.com>
2016-09-18 13:42:12 -0700

    Add changeLog

--- application/models/Quotes.php
+++ application/models/Quotes.php
@@ -66,4 +66,20 @@ class Quotes extends CI_Model {
             
         }
 
+         public function random()
+        {
+            $this->data['pagebody'] = 'homepage';
+            $source = $this->quotes->all();
+            $author = array();
+            
+            $count = sizeof($source);
+            $authorNum = rand(0, $count - 1);
+            $record = $source[$authorNum];
+            array_push($author, array('who' => $record['who'],
+                'mug' => $record['mug'], 'href' => $record['where'],
+                'what' => $record['what']));
+            $this->data['authors']=$author;
+            $this->render();
+            
+        }
 }
